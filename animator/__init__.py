a69543dd095dbf961d07d40ef129c3169ba3de35
gujoh <gustafjohanson@live.com>
2024-03-19 12:46:51 +0100

    fix

---
+++ animator/__init__.py
@@ -0,0 +1,1 @@
+from animator.animator import Animator, Plotter
--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
-from setuptools import setup
+from distutils.core import setup
 
 setup(name="animator",
       packages=["animator"],
+      version=0.1,
+      url="https://github.com/gujoh/animator",
       install_requires=["matplotlib"],
       include_package_data=True)
